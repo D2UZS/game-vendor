6b738317afbfdcc6e69727c05daf64294f2edf6a
D2UZS <ulozasd@mail.ru>
2022-03-15 12:50:41 +0300

    подключаю кастомный селект

---
+++ templates/default/src/components/app-select/app-select.js
@@ -0,0 +1,12 @@
+import Choices from "choices.js";
+
+// Стандартный селект
+const baseSelects = document.querySelectorAll(".js-choices");
+for (const select of baseSelects) {
+    new Choices(select, {
+        searchEnabled: false, // поле для поиска. включено по дефолту
+        searchPlaceholderValue: "Поиск", // плейсхолдер для поля поиска
+        placeholder: true,
+        itemSelectText: "", // Текст, который отображается, когда пользователь наводит курсор на выбранный вариант. При включении этой настройки нужно добавить padding-right у селектора .choices__list--dropdown .choices__item--selectable в файле select.scss
+    });
+}
